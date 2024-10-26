71504e7baf2b7c32255b272df0bcd83798008a88
vidm27 <davidmoran464@gmail.com>
2024-10-26 14:09:54 -0500

    feat(transcription):integrar la serializacion de segmentos y state
    Se añade los segmentos y state para centralizar la serializacion de la transcripción

--- server/transcription/serializers.py
+++ server/transcription/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
+
 from .models import Transcription, Segment, State
 
-
-class TranscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transcription
-        fields = '__all__'
 
 class SegmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +12,13 @@ class SegmentSerializer(serializers.ModelSerializer):
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
+        fields = '__all__'
+
+
+class TranscriptionSerializer(serializers.ModelSerializer):
+    state = serializers.StringRelatedField(read_only=True)
+    segments = SegmentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Transcription
         fields = '__all__'
