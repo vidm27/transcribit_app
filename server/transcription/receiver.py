60d9488ba7db234715a7804737cadf9ad86a5ff6
vidm27 <davidmoran464@gmail.com>
2024-10-26 14:13:44 -0500

    feat(transcription):Agregar el cambio de estado de la transcripción
    Si la transcripción es exitosa, se actualiza el estado de la transcripción, en caso contrario se marca el cambio de estado como fallido.

--- server/transcription/receiver.py
+++ server/transcription/receiver.py
@@ -1,3 +1,4 @@
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from faster_whisper import WhisperModel
@@ -30,21 +31,26 @@ def transcribe_audio(sender, instance, created, **kwargs) -> None:
         for segment in current_segments:
             logger.info(f"Segment: {segment}")
             segments_format.append(SegmentModel(start=segment.start, end=segment.end, text=segment.text))
+        try:
+            transcription = TranscriptionModel(
+                language=info.language,
+                language_probability=info.language_probability,
+                duration=info.duration,
+                segments=segments_format
+            )
+            instance.language = transcription.language
+            instance.language_probability = transcription.language_probability
+            instance.duration = float_to_timefield(transcription.duration)
+            instance.state = 2
+            instance.save()
 
-        transcription = TranscriptionModel(
-            language=info.language,
-            language_probability=info.language_probability,
-            duration=info.duration,
-            segments=segments_format
-        )
-        instance.language = transcription.language
-        instance.language_probability = transcription.language_probability
-        instance.duration = float_to_timefield(transcription.duration)
-        instance.save()
-
-        logger.info(f"Create {len(segments_format)} segments")
-        segments_format = [Segment(transcription_id=instance.id, minutes_start=segment.start, minutes_end=segment.end,
-                                   segment_original=segment.text) for segment in transcription.segments]
-        Segment.objects.bulk_create(segments_format)
-        logger.debug(f"Created {len(segments_format)} segments")
+            logger.info(f"Create {len(segments_format)} segments")
+            segments_format = [Segment(transcription_id=instance.id, minutes_start=segment.start, minutes_end=segment.end,
+                                       segment_original=segment.text) for segment in transcription.segments]
+            Segment.objects.bulk_create(segments_format)
+            logger.debug(f"Created {len(segments_format)} segments")
+        except Exception as e:
+            logger.error(f"Error al crear la transcripción: {e}")
+            instance.state = 3
+            instance.save()
 
