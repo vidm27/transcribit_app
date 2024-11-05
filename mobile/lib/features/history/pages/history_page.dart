c8e64c03ed22470bda5c4953d2ebadca41008c93
vidm27 <davidmoran464@gmail.com>
2024-11-04 22:01:42 -0500

    feat(transcription): Se agrega la gestion del infinete scroll pa el historial

--- mobile/lib/features/history/pages/history_page.dart
+++ mobile/lib/features/history/pages/history_page.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:transcribit_app/features/history/providers/history_provider.dart';
 
-class HistoryPage extends StatelessWidget {
+class HistoryPage extends ConsumerStatefulWidget {
   const HistoryPage({super.key});
 
   @override
+  ConsumerState<HistoryPage> createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends ConsumerState<HistoryPage> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      ref.read(historyNotifierProvider.notifier).loadNextPage();
+    });
+    initListenerScroll();
+  }
+
+  void initListenerScroll() {
+    scrollController.addListener(() {
+      if (ref.read(historyNotifierProvider.notifier).isLoading) return;
+
+      if ((scrollController.position.pixels + 200) >=
+          scrollController.position.maxScrollExtent) {
+        ref.read(historyNotifierProvider.notifier).loadNextPage();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final historyState = ref.watch(historyNotifierProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historial'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: historyState.length,
+                itemBuilder: (context, index) {
+                  final transcription = historyState[index];
+                  return ListTile(
+                    onTap: () {
+                      context.push("/transcription/${transcription.id}");
+                    },
+                    title: Text(transcription.title ?? "No tiene título"),
+                    subtitle: Text(transcription.language ?? "No tiene idioma"),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ mobile/lib/features/history/providers/history_provider.dart
@@ -0,0 +1,58 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:transcribit_app/config/api/api_config.dart';
+import 'package:transcribit_app/features/transcription/models/transcription_response_model.dart';
+
+final historyProvider =
+    FutureProvider.autoDispose<TranscriptionResponse>((ref) async {
+  final dio = Dio(apiConfig);
+  final response = await dio.get('/transcription/');
+
+  if (response.statusCode != 200) {
+    log("Error getting history: ${response.statusCode}", level: 2);
+    throw Exception('Failed to load history');
+  }
+  // log("History loaded: ${response.data}", level: 2);
+  return TranscriptionResponse.fromJson(response.data);
+});
+
+class HistoryNotifier extends StateNotifier<List<Transcription>> {
+  HistoryNotifier() : super([]);
+
+  int currentPage = 0;
+  bool isLoading = false;
+  bool hasNextPage = false;
+
+  Future<void> loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+
+    if (hasNextPage == false && currentPage != 0) {
+      isLoading = false;
+      return;
+    }
+    currentPage++;
+    log("Current page: $currentPage", level: 2);
+    final dio = Dio(apiConfig);
+    final response = await dio.get('/transcription/?page=$currentPage');
+    final transcription = TranscriptionResponse.fromJson(response.data);
+    if (response.statusCode != 200) {
+      log("Error getting history: ${response.statusCode}", level: 2);
+      throw Exception('Failed to load history');
+    }
+    if (transcription.next != null) {
+      hasNextPage = true;
+    } else {
+      hasNextPage = false;
+    }
+    // log("History loaded: ${response.data}", level: 2);
+    state = [...state, ...transcription.results];
+    isLoading = false;
+  }
+}
+
+final historyNotifierProvider =
+    StateNotifierProvider<HistoryNotifier, List<Transcription>>(
+        (ref) => HistoryNotifier());
